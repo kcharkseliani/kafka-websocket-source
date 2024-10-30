1c9c1806d8640976072d270d1b0a84fd2a76ff52
Konstantin <k.charkseliani@protonmail.com>
2024-10-30 21:19:56 +0000

    Initialised maven project

---
+++ src/main/java/com/kcharkseliani/kafka/connect/websocket/App.java
@@ -0,0 +1,13 @@
+package com.kcharkseliani.kafka.connect.websocket;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
